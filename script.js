66f32d33f3c881d55034ede91548210f0f08a735
josefinaossvald <josefinaossvald@gmail.com>
2024-06-11 18:36:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('icono').addEventListener('click', function() {
+    var formulario = document.getElementById('formulario');
+    formulario.style.display = formulario.style.display === 'none' ? 'block' : 'none';
+  });
+  
